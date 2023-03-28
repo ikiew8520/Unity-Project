ef7da98b995686c9cdeb16ec9c1e9de2ff9ffc07
ikiew8520 <ikiew_555@hotmail.com>
2023-03-28 21:19:47 +0700

    Only buff with map
    Only buffalo
    with map
    and C# code to control the buffalo

---
+++ CP214 Project/Assets/Script/CarController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+
+    private float moveInput;
+    private float turnInput;
+
+    public float fwdSpeed;
+    public float revSpeed;
+    public float turnSpeed;
+
+    public Rigidbody sphereRB;
+
+    void Start()
+    {
+        sphereRB.transform.parent = null;
+    }
+
+
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Vertical");
+        turnInput = Input.GetAxisRaw("Horizontal");
+
+
+        moveInput *= moveInput > 0 ? fwdSpeed : revSpeed;
+
+        if(moveInput > 0)
+        {
+            moveInput *= fwdSpeed;
+        }
+        else
+        {
+            moveInput *= revSpeed;
+        }
+        
+
+        transform.position = sphereRB.transform.position;
+
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+        transform.Rotate(0, newRotation, 0, Space.World);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+    }
+}
